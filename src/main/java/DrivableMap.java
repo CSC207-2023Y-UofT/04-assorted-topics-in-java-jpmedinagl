b4dcb8c876cf70e1cdf97f3f421bdf2f2ed6f6c3
jpmedinagl <jpmedinagl@gmail.com>
2023-05-21 12:49:55 -0400

    adding drivable map complete

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -28,8 +28,22 @@ class DrivableMap {
      *       Return true if the Drivable was added to drivable_map.
      */
 
-
-
+    /**
+     * Return boolean whether a drivable object has been added
+     * to the map. A drivable object is added if the id associated with
+     * the object does not appear as a key in the map.
+     *
+     * @param id key associated with the object
+     * @param object to be added to the map
+     * @return boolean of whether the object was added to the map
+     */
+    public boolean addDrivable(String id, Drivable object) {
+        if (!drivable_map.containsKey(id)) {
+            drivable_map.put(id, object);
+            return true;
+        }
+        return false;
+    }
 
     /* TODO: Write a method named hasFasterThan that takes an int (a speed)
      *       and returns true iff there is at least one item in drivable_map
@@ -38,16 +52,41 @@ class DrivableMap {
      * iterate through drivable_map.
      */
 
-
-
-
+    /**
+     * Return a boolean whether a Drivable object has a maxSpeed faster than
+     * a speed inputted.
+     *
+     * @param speed to compare to
+     * @return true if there is at least one item in drivable_map that has a
+     * maxSpeed greater than or equal to the speed given
+     */
+    public boolean hasFasterThan(int speed) {
+        for (Drivable value : drivable_map.values()) {
+            if (value.getMaxSpeed() >= speed) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     /* TODO: Write a method named getTradable that takes no arguments and
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
 
-
-
-    
+    /**
+     * Takes no arguments and returns a list of all the Tradable items in
+     * the map.
+     *
+     * @return list of all the Tradable items in drivable_map
+     */
+    public List<Tradable> getTradable() {
+        List<Tradable> tradableItems = new ArrayList<Tradable>();
+        for (Drivable value : drivable_map.values()) {
+            if (value instanceof Tradable) {
+                tradableItems.add((Tradable) value);
+            }
+        }
+        return tradableItems;
+    }
 }
